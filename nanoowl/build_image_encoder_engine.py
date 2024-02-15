4bf41fa028f763d6712bb9f9716f9c96e1729ea0
Michael Fulton <michael.scott.fulton@gmail.com>
2024-02-15 14:51:58 -0500

    Update build_image_encoder_engine.py

--- nanoowl/build_image_encoder_engine.py
+++ nanoowl/build_image_encoder_engine.py
@@ -22,9 +22,9 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("output_path", type=str)
+    parser.add_argument("onnx_path", type=str)
     parser.add_argument("--model_name", type=str, default="google/owlvit-base-patch32")
     parser.add_argument("--fp16_mode", type=bool, default=True)
-    parser.add_argument("--onnx_opset", type=int, default=16)
     args = parser.parse_args()
     
     predictor = OwlPredictor(
@@ -33,6 +33,6 @@ if __name__ == "__main__":
 
     predictor.build_image_encoder_engine(
         args.output_path,
+        args.onnx_path,
         fp16_mode=args.fp16_mode,
-        onnx_opset=args.onnx_opset
     )
